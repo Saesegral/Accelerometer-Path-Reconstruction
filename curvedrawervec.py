977d3941a9e392cb7a75d32dc8b7ce78d7170eee
Saesegral <stevenkgrigsby@gmail.com>
2019-01-24 12:58:14 -0600

    Improved Readability and Functionality
    I moved things over to using vectors, adjusted the net acceleration to be 0 to try to cut down on drift. Improved the integration algorithm slightly. Overall fixed the function definitions to remove repetition and improve the readability of the code.

--- curvedrawervec.py
+++ curvedrawervec.py
@@ -1,49 +1,19 @@
-import bpy, csv, math, numpy
+import bpy, csv, mathutils
 
+from mathutils import Vector
 
-#name = "figure8"
-#name = "walkingaround"
-#name = "chair"
-#name = "toss"
-#name = "nothing"
-name = "circle10"
+def getAccelData(fp, threshold=10):
+    #only collects data after the threshold, they tend to be off
 
-
-#Need to fix the file path so that it's not dependent to my computer
-fp = "C:/Users/Clear/Documents/GitHub/Accelerometer-Path-Reconstruction/Paths/" + name +".tsv"
-
-
-with open(fp) as tsvfile:
-    rdr = csv.reader(tsvfile, delimiter='\t')
-    ts=[]
-    accels=[]
-    for i, row in enumerate( rdr ):
-        #remove the first few pieces of data, they tend to be off
-        if i <= 5: continue
-        ts.append(float(row[1]))
-        accels.append([float(n) for n in row[2:5]])
-
-
-
-
-
-#Subtracts the first entry from all the points
-def initialize(points):
-  if isinstance(points, float):
-    return [p-points[0] for p in points]
-  return [tuple(numpy.subtract(p, points[0])) for p in points]
-    
-def delta(points):
-    deltas=[]
-    for i in range(len(points)-1):
-        deltas.append(points[i+1]-points[i])
-    return deltas
-
-def avg(points):
-    avgs=[]
-    for i in range(len(points)-1):
-        avgs.append([p/2 for p in tuple(numpy.add(points[i+1],points[i]))])
-    return avgs
+    with open(fp) as tsvfile:
+        rdr = csv.reader(tsvfile, delimiter='\t')
+        ts=[]
+        accels=[]
+        for i, row in enumerate( rdr ):
+            if i <= threshold: continue
+            ts.append(float(row[1]))
+            accels.append(Vector([float(n) for n in row[2:5]]))
+    return accels,ts
 
 def makePathEdges(points):
     edges=[]  
@@ -52,48 +22,53 @@ def makePathEdges(points):
     return edges
 
 def integrate(vecs, times):
-    dt=delta(times)
-    average=avg(vecs)
-    res=[(0.0,0.0,0.0)]
+    res=[Vector((0.0,0.0,0.0))]
     for i in range(len(times)-1):
-        res.append(tuple(numpy.add(res[-1],tuple([v*dt[i] for v in average[i]]))))
+        res.append(res[-1]+(vecs[i+1]+vecs[i])/2*(times[i+1]-times[i]))
     return res
+    
+def makeCurve(curveName, points):
+    profile_mesh = bpy.data.meshes.new(curveName)
+    profile_mesh.from_pydata(points, makePathEdges(points), [])
+    profile_mesh.update()
+    profile_object = bpy.data.objects.new(curveName, profile_mesh)
+    profile_object.data = profile_mesh  # this line is redundant .. it simply overwrites .data
+    scene = bpy.context.collection
+    scene.objects.link(profile_object)
+    
+def makeCurves(curveName, accelVecs, times):
+    #Subtracts off the first acceleration from all accelerations to counteract gravity.
+    #accelVecs = [a-accelVecs[0] for a in accelVecs]
+    #Subtracts off the average acceleration from all accelerations to counteract gravity.
+    avg = Vector((0.0,0.0,0.0))
+    for a in accelVecs:
+        avg+=a
+    avg/=len(accelVecs)
+    accelVecs = [a-avg for a in accelVecs]
+    velVecs = integrate(accelVecs, times)
+    posVecs = integrate(velVecs, times)
+    makeCurve("Acceleration "+curveName, accelVecs)
+    makeCurve("Velocity "+curveName, velVecs)
+    makeCurve("Position "+curveName, posVecs)
 
 
 
-accels = initialize(accels)
-accelEdges=makePathEdges(accels)
+#The suffix 04, 10, 20, and 40 represent the approximate Hz of the polling data.
 
-vels = integrate(accels, ts)
-velEdges = makePathEdges(vels)
+#name = "figure8"
+#name = "walkingaround"
+#name = "chair"
+#name = "toss"
+#name = "nothing"
+#name = "circle10"
+#name = "arc04"
+#name = "singlecircle"
+#name = "knot10"
+name = "fastcircle10"
 
-poss = integrate(vels, ts)
-posEdges = makePathEdges(poss)
+#Need to fix the file path so that it's not dependent to my computer
+fp = "C:/Users/Clear/Documents/GitHub/Accelerometer-Path-Reconstruction/Paths/" + name +".tsv"
 
+accelVecs, ts = getAccelData(fp)
 
-
-
-
-profile_mesh = bpy.data.meshes.new("Acceleration "+name)
-profile_mesh.from_pydata(accels, accelEdges, [])
-profile_mesh.update()
-profile_object = bpy.data.objects.new("Acceleration "+name, profile_mesh)
-profile_object.data = profile_mesh  # this line is redundant .. it simply overwrites .data
-scene = bpy.context.collection
-scene.objects.link(profile_object)
-
-profile_mesh = bpy.data.meshes.new("Velocity "+name)
-profile_mesh.from_pydata(vels, velEdges, [])
-profile_mesh.update()
-profile_object = bpy.data.objects.new("Velocity "+name, profile_mesh)
-profile_object.data = profile_mesh  # this line is redundant .. it simply overwrites .data
-scene = bpy.context.collection
-scene.objects.link(profile_object)
-
-profile_mesh = bpy.data.meshes.new("Position "+name)
-profile_mesh.from_pydata(poss, posEdges, [])
-profile_mesh.update()
-profile_object = bpy.data.objects.new("Position "+name, profile_mesh)
-profile_object.data = profile_mesh  # this line is redundant .. it simply overwrites .data
-scene = bpy.context.collection
-scene.objects.link(profile_object)
+makeCurves(name, accelVecs, ts)
